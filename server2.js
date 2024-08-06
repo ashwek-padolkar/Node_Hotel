047acecf7ecde0e8f57b0022f50526f18c3b2090
AshwekPadolkar <padolkarashwek11@gmail.com>
2024-08-06 12:20:51 +0530

    update

--- server2.js
+++ server2.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 // Middleware to parse JSON bodies
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());                   // req.body
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
